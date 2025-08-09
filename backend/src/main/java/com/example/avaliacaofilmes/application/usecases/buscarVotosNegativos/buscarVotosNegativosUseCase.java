042565d14c9e6d21c0d40f9b074fb2983d588e79
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-08-09 15:11:19 -0300

    fix: faltou a anotação de service

--- backend/src/main/java/com/example/avaliacaofilmes/application/usecases/buscarVotosNegativos/buscarVotosNegativosUseCase.java
+++ backend/src/main/java/com/example/avaliacaofilmes/application/usecases/buscarVotosNegativos/buscarVotosNegativosUseCase.java
@@ -2,7 +2,9 @@ package com.example.avaliacaofilmes.application.usecases.buscarVotosNegativos;
 
 import com.example.avaliacaofilmes.domain.repositories.IFilmesRepository;
 import com.example.avaliacaofilmes.infra.web.DTOs.BuscarVotosResponseDTO;
+import org.springframework.stereotype.Service;
 
+@Service
 public class buscarVotosNegativosUseCase implements IBuscarVotosNegativosUseCase{
 
     private final IFilmesRepository repository;
--- backend/src/main/java/com/example/avaliacaofilmes/application/usecases/buscarVotosPositivos/buscarVotosPositivosUseCase.java
+++ backend/src/main/java/com/example/avaliacaofilmes/application/usecases/buscarVotosPositivos/buscarVotosPositivosUseCase.java
@@ -2,7 +2,9 @@ package com.example.avaliacaofilmes.application.usecases.buscarVotosPositivos;
 
 import com.example.avaliacaofilmes.domain.repositories.IFilmesRepository;
 import com.example.avaliacaofilmes.infra.web.DTOs.BuscarVotosResponseDTO;
+import org.springframework.stereotype.Service;
 
+@Service
 public class buscarVotosPositivosUseCase implements IBuscarVotosPositivosUseCase{
 
     private final IFilmesRepository repository;
